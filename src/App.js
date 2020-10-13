df8b243ab9189fe8a9aab72690842992276dc8d4
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-13 12:46:19 -0400

    adds sass styling files for typography, color, and sizing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.sass';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h3>Candle Two</h3>
+      <h2>Announcements</h2>
+      <p>No birthdays this month</p>
+      <h2>All Birthdays</h2>
+      <p>9/23 Mariana turns 25</p>
+      <p>9/22 Meredith turns 26</p>
+      <h2>Calendar</h2>
+      <h1>+</h1>
     </div>
   );
 }
